e87e3b171518f63e465f689d2065ad7802be21d5
Pedro Lugon <pedromotta20@gmail.com>
2025-01-10 17:16:10 -0300

    feat: add everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo, useState } from "react";
+import { Textarea } from "./components/ui/textarea";
+import Footer from "./components/footer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState<string>("");
+
+  const countJustLettersAndNumbers = (str: string) => {
+    return str.replace(/[^a-zA-Z0-9]/g, "").length;
+  };
+
+  const countWords = (str: string) => {
+    return str.trim().split(/\s+/).filter(Boolean).length;
+  };
+
+  const countSentences = (str: string) => {
+    return (str.match(/[^.!?]*[.!?]/g) || []).length;
+  };
+
+  const countParagraphs = (str: string) => {
+    return str.split(/\n+/).filter(Boolean).length;
+  };
+
+  const countSpaces = (str: string) => {
+    return (str.match(/ /g) || []).length;
+  };
+
+  const characters = useMemo(() => countJustLettersAndNumbers(text), [text]);
+  const words = useMemo(() => countWords(text), [text]);
+  const sentences = useMemo(() => countSentences(text), [text]);
+  const paragraphs = useMemo(() => countParagraphs(text), [text]);
+  const spaces = useMemo(() => countSpaces(text), [text]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="p-4 min-h-screen">
+        <div className="mb-4">
+          <h1 className="text-2xl font-bold">Conta Letras</h1>
+          <Textarea
+            placeholder="Digite aqui seu texto"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            rows={20}
+          />
+        </div>
+        <div className="flex flex-col gap-4 md:flex-row md:justify-center">
+          <div>
+            <p className="label">Caracteres</p>
+            <span>{characters}</span>
+          </div>
+          <div>
+            <p className="label">Palavras</p>
+            <span>{words}</span>
+          </div>
+          <div>
+            <p className="label">Sentenças</p>
+            <span>{sentences}</span>
+          </div>
+          <div>
+            <p className="label">Paragráfos</p>
+            <span>{paragraphs}</span>
+          </div>
+          <div>
+            <p className="label">Espaços</p>
+            <span>{spaces}</span>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="text-center bg-foreground text-background p-4">
+      <p>
+        Desenvolvido com ❤️ por{" "}
+        <a href="https://lugon.dev" className="underline">
+          Pedro Lugon
+        </a>
+      </p>
+    </footer>
+  );
+}
